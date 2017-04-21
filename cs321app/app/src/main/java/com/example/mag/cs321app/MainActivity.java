c9d139469a8a60e4500024e10d052327c3a83516
maggieli8567@gmail.com <maggieli8567@gmail.com>
2017-04-21 14:37:45 -0400

    Sign in page

--- cs321app/app/src/main/java/com/example/mag/cs321app/MainActivity.java
+++ cs321app/app/src/main/java/com/example/mag/cs321app/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.*;
@@ -17,9 +18,10 @@ import com.google.firebase.auth.*;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText mvalueField;   // Email
-    private Button maddBtn;         // login button
-    private EditText mKeyValue;     // Username
+
+    private EditText mEmailFied;      // Email
+    private Button mlogInBtn;         // login button
+    private EditText mPasswordField;  // Password
     private Button msignUpBtn;
 
     private Firebase mRef;
@@ -37,46 +39,10 @@ public class MainActivity extends AppCompatActivity {
 
         mAuth = FirebaseAuth.getInstance();
 
-        mvalueField = (EditText) findViewById(R.id.valueField);
-        maddBtn = (Button) findViewById(R.id.addBtn);
-        mKeyValue = (EditText) findViewById(R.id.keyValue);
+        mEmailFied = (EditText) findViewById(R.id.emailField);
+        mlogInBtn = (Button) findViewById(R.id.loginBtn);
+        mPasswordField = (EditText) findViewById(R.id.passwordField);
 
-
-
-
-        // login button
-        maddBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                // start signing in
-                String password = mvalueField.getText().toString(); //password (value in tutorial)
-                String username = mKeyValue.getText().toString(); //username (key in tutorial)
-                Firebase mRefChild = mRef.child(username);
-
-                mRef.setValue(password);
-
-
-                if(TextUtils.isEmpty(username) || TextUtils.isEmpty(password)){
-
-                    Toast.makeText(MainActivity.this,"Fields are empty", Toast.LENGTH_LONG).show();
-
-                } else {
-                    mAuth.signInWithEmailAndPassword(username, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            if(!task.isSuccessful()){
-                                Toast.makeText(MainActivity.this,"Cannot Sign In", Toast.LENGTH_LONG).show();
-                            }
-                        }
-                    });
-                }
-
-
-
-
-            }
-        });
 
         // Set up an AuthStateListener that responds to changes in the user's sign-in state
         mAuthListener = new FirebaseAuth.AuthStateListener() {
@@ -84,11 +50,41 @@ public class MainActivity extends AppCompatActivity {
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
 
                 // when there is a user active, he will be directed to the account activity page
-                if(firebaseAuth.getCurrentUser() != null) {
-                    startActivity(new Intent(MainActivity.this, AccountAccivity.class));
+                if(firebaseAuth.getCurrentUser() == null) {
+
+                    // This line is for testing purpose, can be deleted later
+                    Toast.makeText(MainActivity.this, "BYE.", Toast.LENGTH_LONG).show();
                 }
+
+                else if(firebaseAuth.getCurrentUser() != null) {
+
+                    Toast.makeText(MainActivity.this, "HELLO.", Toast.LENGTH_LONG).show();
+
+                    // create a new Intent and go to the Home page
+                    // ***** AccountActivity page is changed
+                    startActivity(new Intent(MainActivity.this, AccountAccivity.class));
+
+                    // sign out of the system
+                    firebaseAuth.signOut();
+
+                }
+
             }
         };
+
+
+
+        // login button
+        mlogInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                startSignIn();
+
+            }
+        });
+
+
 
 
         msignUpBtn = (Button) findViewById(R.id.signUpBtn);
@@ -104,11 +100,34 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    //@Override
-    //protected void onStart(){
-      //  super.onStart();
+    @Override
+    protected void onStart() {
+        super.onStart();
 
-      //  mAuth.addAuthStateListener(mAuthListener);
-    //}
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    private void startSignIn() {
+        String email = mEmailFied.getText().toString();
+        String password = mPasswordField.getText().toString();
+
+
+        if(TextUtils.isEmpty(email) || TextUtils.isEmpty(password)) {
+
+            Toast.makeText(MainActivity.this, "Fields are empty.", Toast.LENGTH_LONG).show();
+
+        } else {
+
+            mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>(){
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task){
+                    if(!task.isSuccessful()){
+                        Toast.makeText(MainActivity.this, "Cannot Sign in", Toast.LENGTH_LONG).show();
+                    }
+                }
+            });
+
+        }
+    }
 
 }
