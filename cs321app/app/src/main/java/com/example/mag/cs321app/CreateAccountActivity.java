de340fd0b09c679c3f4aa6ece1735f73e37d797f
Eunice Wang <eunicewang1995@gmail.com>
2017-04-21 11:40:36 -0400

    editted create account page

--- cs321app/app/src/main/java/com/example/mag/cs321app/CreateAccountActivity.java
+++ cs321app/app/src/main/java/com/example/mag/cs321app/CreateAccountActivity.java
@@ -3,6 +3,8 @@ package com.example.mag.cs321app;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.firebase.client.Firebase;
+
 public class CreateAccountActivity extends AppCompatActivity {
 
     @Override
@@ -11,3 +13,10 @@ public class CreateAccountActivity extends AppCompatActivity {
         setContentView(R.layout.activity_create_account);
     }
 }
+    // start signing in
+    //save info to firebase
+   /* String password = mvalueField.getText().toString(); //password (value in tutorial)
+    String username = mKeyValue.getText().toString(); //username (key in tutorial)
+    Firebase mRefChild = mRef.child(username);
+
+    mRef.setValue(password);*/
