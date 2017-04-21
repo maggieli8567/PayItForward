4d99c1de897679cdbef2f5cc55b08c310ebcc8f7
pprimee <hphan13@masonlive.gmu.edu>
2017-04-21 13:50:22 -0400

    main_menu new item

--- cs321app/app/src/main/java/com/example/mag/cs321app/TimelineActivity.java
+++ cs321app/app/src/main/java/com/example/mag/cs321app/TimelineActivity.java
@@ -9,10 +9,12 @@ public class TimelineActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // get layout from timeline_main
         setContentView(R.layout.timeline_main);
     }
 
     public boolean onCreateOptionsMenu(Menu menu){
+        // get button pics from main_menu for timeline add button
         getMenuInflater().inflate(R.menu.main_menu,menu);
 
         return super.onCreateOptionsMenu(menu);
