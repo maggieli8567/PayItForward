feceda08afbb14d0c403a47765b416f7161ba9d9
pprimee <hphan13@masonlive.gmu.edu>
2017-04-23 04:22:12 -0400

    postactivity button and inputs created/need contraints fixed

--- cs321app/app/src/main/java/com/example/mag/cs321app/PostActivity.java
+++ cs321app/app/src/main/java/com/example/mag/cs321app/PostActivity.java
@@ -2,12 +2,48 @@ package com.example.mag.cs321app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
+
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import android.widget.EditText;
+import android.widget.Button;
+import android.view.View.OnClickListener;
 
 public class PostActivity extends AppCompatActivity {
+    // Pam fields for PostActivity
+    private EditText mPostTitle;
+    private EditText mPostDescription;
 
+    private Button mSubmitButton;
+    private StorageReference mStorage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
+
+        // Firebase storage root directory
+        mStorage = FirebaseStorage.getInstance().getReference();
+
+        mPostTitle = (EditText) findViewById(R.id.post_title);
+        mPostDescription = (EditText) findViewById(R.id.post_description);
+        mSubmitButton = (Button) findViewById(R.id.post_button);
+
+mSubmitButton.setOnClickListener(new View.OnClickListener(){
+    @Override
+    public void onClick(View view){
+        startPosting();
     }
+        });
+
+    }
+    private void startPosting(){
+        String title_val = mPostTitle.getText().toString().trim();
+        String desc_val = mPostDescription.getText().toString().trim();
+    }
+
+
+
+
 }
